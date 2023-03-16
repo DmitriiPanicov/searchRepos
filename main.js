5220f6d731a05ccfab4cc2992afd73e8ea136088
DmitriiPanicov <126007399+DmitriiPanicov@users.noreply.github.com>
2023-03-17 00:33:01 +0400

    fix timeConverter function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+let repositories = [];
+
 async function searchRepos() {
 
-    let repositories = [];
     let searchRequest = document.getElementById('searchRequest').value;
     let response = await fetch(`https://api.github.com/search/repositories?q=${searchRequest}&per_page=10`);
 
@@ -9,7 +10,8 @@ async function searchRepos() {
         let searchResult = await response.json();
 
         if (searchResult.total_count !== 0) {
-
+            
+            repositories = [];
             searchResult.items.forEach(item => {
                 let repository = {
                     name: item.name,
@@ -25,64 +27,62 @@ async function searchRepos() {
             showRepositories();
 
         } else {
-            showFailureMessage();
+            showFailureMessage(searchRequest);
         }
 
     } else {
         showServerResponse(response.status);
     }
+}
 
-    function showRepositories() {
+function showRepositories() {
 
-        let repositoriesField = document.getElementsByClassName('repositories-field')[0];
-        repositoriesField.replaceChildren();
-        let out = '';
+    let repositoriesField = document.getElementsByClassName('repositories-field')[0];
+    repositoriesField.replaceChildren();
+    let out = '';
 
-        repositories.forEach(function (item) {
-            out += `
-            <div class="repository-item">
-                <p><b>Репозиторий:</b> <a href=${item.html_url} target="_blank">${item.name}</a></p>
-                <p><b>Дата создания:</b> ${item.created_at.getFullYear()}</p>
-                <p><b>Описание:</b> ${item.description !== null ? item.description : 'описание отсутствует'}</p>
-                <p><b>Используемый язык:</b> ${item.language !== null ? item.language : 'не знаем мы таких языкаф'}</p>
-                <p><b>Количество просмотров:</b> ${item.watchers_count}</p>
-            </div>
-        `;
-        });
-        repositoriesField.innerHTML = out;
-    }
-
-    function showServerResponse(response) {
-
-        let repositoriesField = document.getElementsByClassName('repositories-field')[0];
-        repositoriesField.replaceChildren();
-        let out = '';
-
+    repositories.forEach(function (item) {
         out += `
-            <div class="repository-item">
-                <p><b>Ответ сервера:</b> ${response}</p>
-            </div>
-        `;
+        <div class="repository-item">
+            <p><b>Репозиторий:</b> <a href=${item.html_url} target="_blank">${item.name}</a></p>
+            <p><b>Дата создания:</b> ${item.created_at.getFullYear()}</p>
+            <p><b>Описание:</b> ${item.description !== null ? item.description : 'описание отсутствует'}</p>
+            <p><b>Используемый язык:</b> ${item.language !== null ? item.language : 'не знаем мы таких языкаф'}</p>
+            <p><b>Количество просмотров:</b> ${item.watchers_count}</p>
+        </div>
+    `;
+    });
+    repositoriesField.innerHTML = out;
+}
 
-        repositoriesField.innerHTML = out;
-    }
+function showServerResponse(response) {
 
-    function showFailureMessage() {
+    let repositoriesField = document.getElementsByClassName('repositories-field')[0];
+    repositoriesField.replaceChildren();
+    let out = '';
 
-        let repositoriesField = document.getElementsByClassName('repositories-field')[0];
-        repositoriesField.replaceChildren();
-        let out = '';
+    out += `
+        <div class="repository-item">
+            <p>Что-то пошло не так!</p>
+            <p><b>Ответ сервера:</b> ${response}</p>
+        </div>
+    `;
 
-        out += `
-            <div class="repository-item">
-                <p>Упс!</p>
-                <p>По запросу <b>${searchRequest}</b> ничего не найдено</p>
-            </div>
-        `;
+    repositoriesField.innerHTML = out;
+}
 
-        repositoriesField.innerHTML = out;
-    }
-    
+function showFailureMessage(searchRequest) {
 
+    let repositoriesField = document.getElementsByClassName('repositories-field')[0];
+    repositoriesField.replaceChildren();
+    let out = '';
 
+    out += `
+        <div class="repository-item">
+            <p>Упс!</p>
+            <p>По запросу <b>${searchRequest}</b> ничего не найдено</p>
+        </div>
+    `;
+
+    repositoriesField.innerHTML = out;
 }
